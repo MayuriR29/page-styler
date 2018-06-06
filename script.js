2c7c050398d61160e73641954481f27ff8e562f8
MayuriR29 <mayuri.gr@gmail.com>
2018-06-06 16:15:32 +0800

    added changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("Hello");
+// console.log('good evening');
+// console.log(20+2);
+// console.log('1'+'24');
+// console.log(`javascript`);
+const src="script.js=1;
+const myString='Mayuri';
+let vartwo=2;
+var var3=3;
